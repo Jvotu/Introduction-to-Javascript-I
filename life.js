d99a3b52470e1ba1738e60d9073f6be4e5519832
Jvotu <105566948+Jvotu@users.noreply.github.com>
2022-06-25 16:32:59 +0100

    Zuri task

---
+++ life.js
@@ -0,0 +1,10 @@
+document.write('My name is Votu-Sakpere Jeshua.');
+document.write('<br>');
+document.write('I am 178cm tall.');
+document.write('<br>');
+document.write('I am a Nigerian.');
+
+alert('My name is Votu-Sakpere Jeshua');
+alert('I am 178cm tall');
+alert('I am a Nigerian');
+
